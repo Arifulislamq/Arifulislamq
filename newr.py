bf32b98e839ba667f9223fe52b32cc008e909d10
Md Arif <md7684730@gmail.com>
2025-11-05 19:05:38 +0600

    Update personal information and print statements

--- newr.py
+++ newr.py
@@ -1,22 +1,22 @@
-print("arif")
+print("arifu islam")
 
 
 name = "Ariful islam"
 
-print("my name"+name)
-age =34
-cgpa = 3.45
-original_Year=2002
+print("my name is "+name)
+age =22
+cgpa = 3.12
+original_Year=2001
 original_Month=6
 original_Day=22
 normal_Year=2004
 normal_Month=3
-normal_Day=20
+normal_Day=23
 print("i am",age,"years old.")
 print("my cgpa:",cgpa)
 print("Original birth year:",original_Year)
 print("Original birth month:",original_Month)
 print("Original birth day:",original_Day)
-print("Normal birth year:",normal_Year)
-print("Normal birth month:",normal_Month)
-print("Normal birth day:",normal_Day)
+print("  Normal birth year:",normal_Year)
+print("  Normal birth month:",normal_Month)
+print("  Normal birth day:",normal_Day)
