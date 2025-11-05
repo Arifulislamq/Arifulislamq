402ff8a36b0f79aaf200f680bb8b782f509ecff4
Arif <md0313443@gmail.com>
2025-11-05 17:29:05 +0600

    new

---
+++ newr.py
@@ -0,0 +1,22 @@
+print("arif")
+
+
+name = "Ariful islam"
+
+print("my name"+name)
+age =34
+cgpa = 3.45
+original_Year=2002
+original_Month=6
+original_Day=22
+normal_Year=2004
+normal_Month=3
+normal_Day=20
+print("i am",age,"years old.")
+print("my cgpa:",cgpa)
+print("Original birth year:",original_Year)
+print("Original birth month:",original_Month)
+print("Original birth day:",original_Day)
+print("Normal birth year:",normal_Year)
+print("Normal birth month:",normal_Month)
+print("Normal birth day:",normal_Day)
